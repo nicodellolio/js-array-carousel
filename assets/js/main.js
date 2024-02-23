9163ed12144aecb49a2b9d19529ea2dc645d0ebb
nicodellolio <nico.dellolio18@gmail.com>
2024-02-23 16:51:41 +0100

    add array and loop for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
-console.log('Yes, it works folks!');
+/* tools
+-array
+-loop for
+-log
+-if
+-ternary
+*/
+
+// creiamo il nostro array con le immagini necesserie al carosello
+
+const slides = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp'
+]
+
+// creiamo le immagini in modo dinamico attraverso JS
+
+for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    console.log(slide);
+}
