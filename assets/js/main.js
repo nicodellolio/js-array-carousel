2cd70eca5481a9da3da9b74d9834bf03076c6573
nicodellolio <nico.dellolio18@gmail.com>
2024-02-23 17:28:54 +0100

    add loop for- seek for help

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
 
 // creiamo il nostro array con le immagini necesserie al carosello
 
-const slides = [
+let slides = [
     '01.webp',
     '02.webp',
     '03.webp',
@@ -17,8 +17,38 @@ const slides = [
 ]
 
 // creiamo le immagini in modo dinamico attraverso JS
+let activeSlide = 0;
+
+const slidesElement = document.querySelector('.slides');
+
+const prevElement = document.querySelector('.previous')
+const nextElement = document.querySelector('.next')
+// console.log(prevElement, nextElement);
+
+
 
 for (let i = 0; i < slides.length; i++) {
     const slide = slides[i];
-    console.log(slide);
+    // console.log(slide);
+
+    let imageMarkup = 
+    `<img class="${ i === activeSlide ? 'active' : '' }" src="./assets/img/${slide}">`
+    console.log(imageMarkup);
+
+    slidesElement.insertAdjacentHTML('beforeend', imageMarkup);
 }
+
+prevElement.addEventListener('click', function () {
+    console.log('slide to the previous image');
+} )
+
+nextElement.addEventListener('click', function () {
+    console.log('slide to the next image');
+
+    activeSlide ++
+    console.log(activeSlide);
+
+} )
+
+
+
